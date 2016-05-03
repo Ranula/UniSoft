713a8377c8268529c7b93fbefeb581a1f57befcd
Ranula Liyadipita <ranulapbl@gmail.com>
2016-05-04 00:20:05 +0530

    ReEdited the student class under the project Unisoft

---
+++ Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniSoft
+{
+    class student : Person
+
+    //
+    {
+        private char id;
+        private Parent p;
+
+
+
+        //constuctor to add accept name and the age when creating the object
+
+        public student(string name, string gender, int age, string address, DateTime dateOfAddmission) : base(name, gender, age, address, dateOfAddmission)
+        {
+
+        }
+        // getters
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public Parent getParent()
+        {
+            return p;
+        }
+
+        public void setParent(Parent parent)
+        {
+
+        }
+
+    }
+}
+
+
