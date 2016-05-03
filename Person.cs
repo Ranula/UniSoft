9694147e6be00005beea458fdea7cf0e4e99dc1e
darsana <darshana.14@cse.mrt.ac.lk>
2016-05-03 22:16:26 +0530

    person abtract class added

---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniSoft
+{
+    abstract class Person
+    { 
+    // variables
+        private string name;
+        private string gender;
+        private int age;
+        private string address;
+        private DateTime dateOfAddmission;
+
+    // constroctor
+        public Person(string name,string gender,int age,string address,DateTime dateOfAddmission)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.address = address;
+            this.dateOfAddmission = dateOfAddmission;
+
+        }
+   // setters
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getGender()
+        {
+            return gender;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public DateTime getDateOfAddmission()
+        {
+            return dateOfAddmission;
+        }
+    }
+}
