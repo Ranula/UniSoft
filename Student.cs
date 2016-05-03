d7e9c0b27a3475cf7c6cb950ca21cc990bf903a0
Ranula Liyadipita <ranulapbl@gmail.com>
2016-05-04 00:03:47 +0530

    Rename

--- Student.cs
+++ Student.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniSoft;
 
-namespace UniSoft
+namespace WindowsFormsApplication3
 {
-    class Student
+    class student : Person
+
+    //
     {
+        private char id; 
+        private Parent p;
+       
+
+
+        //constuctor to add accept name and the age when creating the object
+
+        public student(string name, string gender, int age, string address,DateTime dateOfAddmission) : base(name,gender,age,address,dateOfAddmission)
+        {
+
+        }
+        // getters
+       
+        public int getId()
+        {
+            return id;
+        }
+
+        public Parent getParent()
+        {
+            return p;
+        }
+
+        public void setParent(Parent parent)
+        {
+
+        }
+
     }
 }
